5b624984ead9e31313319c470a17c65695589cfb
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-11 23:47:59 +0100

    Init base vars

--- src/main/java/de/aikiit/game/kaiser/Kaiser.java
+++ src/main/java/de/aikiit/game/kaiser/Kaiser.java
@@ -6,4 +6,23 @@ public class Kaiser {
     private Long yield = 0L;
     private Long externalDamage = 0L;
     private Long supplies = 0L;
+
+    ///
+    private Integer zYear; // why z in original?
+    private Long deathToll;
+    private Long deathTollSum; // d1 in original
+    private Long percentDeathToll;
+    private Long increase; // birthRate
+
+    /**
+     * @return area per capita, called <b>L</b> in original.
+     */
+    public Long getAreaPerCapita() {
+        return Long.divideUnsigned(area, population);
+    }
+
+    public Kaiser() {
+        this.population = 95L;
+        this.zYear = 0;
+    }
 }
