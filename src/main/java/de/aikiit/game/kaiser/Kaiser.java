4a8a85edd387a2aa1fa0b504121eb185266a5f80
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-11 23:34:49 +0100

    Base state data

---
+++ src/main/java/de/aikiit/game/kaiser/Kaiser.java
@@ -0,0 +1,9 @@
+package de.aikiit.game.kaiser;
+
+public class Kaiser {
+    private Long population = 0L;
+    private Long area = 0L;
+    private Long yield = 0L;
+    private Long externalDamage = 0L;
+    private Long supplies = 0L;
+}
