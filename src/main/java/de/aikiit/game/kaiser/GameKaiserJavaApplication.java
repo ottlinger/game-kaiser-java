b6ed303c897ef9e9de25e6ae8fab95eb11498130
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-16 00:26:53 +0100

    Refactor

--- src/main/java/de/aikiit/game/kaiser/GameKaiserJavaApplication.java
+++ src/main/java/de/aikiit/game/kaiser/GameKaiserJavaApplication.java
@@ -3,9 +3,9 @@ package de.aikiit.game.kaiser;
 //@SpringBootApplication
 public class GameKaiserJavaApplication {
 
-	public static void main(String[] args) {
-		//SpringApplication.run(GameKaiserJavaApplication.class, args);
-		new KaiserGame().run();
-	}
+    public static void main(String[] args) {
+        //SpringApplication.run(GameKaiserJavaApplication.class, args);
+        new KaiserGame().run();
+    }
 
 }
--- src/main/java/de/aikiit/game/kaiser/KaiserEngine.java
+++ src/main/java/de/aikiit/game/kaiser/KaiserEngine.java
@@ -4,30 +4,21 @@ import lombok.Getter;
 
 @Getter
 public class KaiserEngine {
-    private Long population = 0L;
-    private Long area = 0L;
-    private Long yield = 0L;
     private final Long externalDamage = 0L;
-    private Long supplies = 0L;
-
-    private Long humans = 0L;
-
     ///
     private final Integer zYear; // why z in original?
     private final Long deathToll;
+    private final Long increase; // birthRate?
+    private Long population = 0L;
+    private Long area = 0L;
+    private Long yield = 0L;
+    private Long supplies = 0L;
+    private Long humans = 0L;
     private Long deathTollSum; // d1 in original
     private Long percentDeathToll; // p1 in original
-    private final Long increase; // birthRate?
 
     /// Missing:
     // q - disaster quote
-
-    /**
-     * @return area per capita, called <b>L</b> in original. Land ownership?
-     */
-    public Long getAreaPerCapita() {
-        return Long.divideUnsigned(area, population);
-    }
 
     public KaiserEngine() {
         this.population = 95L;
@@ -39,6 +30,13 @@ public class KaiserEngine {
         this.increase = 5L;
         this.deathToll = 0L;
         this.percentDeathToll = 0L;
+    }
+
+    /**
+     * @return area per capita, called <b>L</b> in original. Land ownership?
+     */
+    public Long getAreaPerCapita() {
+        return Long.divideUnsigned(area, population);
     }
 
 }
