fb7b6617ed04652ee4a9807f8f79c29deaa070af
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-16 10:08:53 +0100

    Fix name of Zentner

--- src/main/java/de/aikiit/game/kaiser/KaiserGame.java
+++ src/main/java/de/aikiit/game/kaiser/KaiserGame.java
@@ -75,7 +75,7 @@ public class KaiserGame {
         System.out.println(KaiserEnginePrinter.ANSI_PURPLE + "#+#+#+#+ Was möchten Sie tun?" + KaiserEnginePrinter.ANSI_RESET);
         System.out.println(KaiserEnginePrinter.ANSI_PURPLE + "Wieviel Land wollen Sie kaufen?" + KaiserEnginePrinter.ANSI_RESET);
         System.out.println(KaiserEnginePrinter.ANSI_PURPLE + "Wieviel Land wollen Sie verkaufen?" + KaiserEnginePrinter.ANSI_RESET);
-        System.out.println(KaiserEnginePrinter.ANSI_PURPLE + "Wieviel DZ wollen Sie an Ihr Volk verteilen?" + KaiserEnginePrinter.ANSI_RESET);
+        System.out.println(KaiserEnginePrinter.ANSI_PURPLE + "Wieviel dzt wollen Sie an Ihr Volk verteilen?" + KaiserEnginePrinter.ANSI_RESET);
         System.out.println(KaiserEnginePrinter.ANSI_PURPLE + "Wieviel Land wollen Sie bebauen?" + KaiserEnginePrinter.ANSI_RESET);
         System.out.println();
     }
