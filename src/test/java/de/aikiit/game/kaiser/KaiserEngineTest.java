d6c02887f8d38ce4e7a81f7a88cb3c8c1543f704
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-16 23:45:22 +0100

    Extract tests

--- src/test/java/de/aikiit/game/kaiser/KaiserEngineTest.java
+++ src/test/java/de/aikiit/game/kaiser/KaiserEngineTest.java
@@ -55,46 +55,4 @@ class KaiserEngineTest {
         assertThat(engine.getCost()).isEqualTo(costB4);
     }
 
-    @Test
-    void buyLandDoesNothingIfZeroIsGiven() {
-        int costB4 = engine.getCost();
-        BigDecimal areaB4 = engine.getArea();
-        BigDecimal supplyB4 = engine.getSupplies();
-
-        engine.buyLand(0L);
-
-        assertThat(engine.getArea()).isEqualTo(areaB4);
-        assertThat(engine.getSupplies()).isEqualTo(supplyB4);
-        assertThat(engine.getCost()).isEqualTo(costB4);
-    }
-
-    @Test
-    void buyLandWithMoreThanCashAsArgumentInducesNoChanges() {
-        int costB4 = engine.getCost();
-        BigDecimal areaB4 = engine.getArea();
-        BigDecimal supplyB4 = engine.getSupplies();
-
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            engine.buyLand(Long.MAX_VALUE);
-        });
-
-        String expectedMessage = "Not Enough Supplies";
-        assertThat(exception.getMessage()).contains(expectedMessage);
-        assertThat(engine.getArea()).isEqualTo(areaB4);
-        assertThat(engine.getSupplies()).isEqualTo(supplyB4);
-        assertThat(engine.getCost()).isEqualTo(costB4);
-    }
-
-    @Test
-    void buyLandRegularUseCaseAndSetPriceToZeroAfterwards() {
-        BigDecimal areaB4 = engine.getArea();
-        BigDecimal supplyB4 = engine.getSupplies();
-        engine.buyLand(4L);
-
-        assertThat(engine.getArea()).isEqualTo(areaB4.add(BigDecimal.valueOf(4L)));
-        assertThat(engine.getSupplies()).isEqualTo(supplyB4.subtract(engine.getYield().multiply(BigDecimal.valueOf(4L))));
-        assertThat(engine.getCost()).isEqualTo(0);
-    }
-
-
 }
