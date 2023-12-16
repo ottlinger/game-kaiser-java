c0741a2f4b3839140dd953d590dbe81fcd352415
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-16 23:58:42 +0100

    Test sell land

---
+++ src/test/java/de/aikiit/game/kaiser/KaiserEngineSellLandTest.java
@@ -0,0 +1,61 @@
+package de.aikiit.game.kaiser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class KaiserEngineSellLandTest {
+    private KaiserEngine engine;
+
+    @BeforeEach
+    void createNewEngineUnderTest() {
+        this.engine = new KaiserEngine();
+    }
+
+    @Test
+    void sellLandDoesNothingIfZeroIsGiven() {
+        int costB4 = engine.getCost();
+        BigDecimal areaB4 = engine.getArea();
+        BigDecimal supplyB4 = engine.getSupplies();
+
+        engine.sellLand(0L);
+
+        assertThat(engine.getArea()).isEqualTo(areaB4);
+        assertThat(engine.getSupplies()).isEqualTo(supplyB4);
+        assertThat(engine.getCost()).isEqualTo(costB4);
+    }
+
+    @Test
+    void sellLandWithNotEnoughLandAsArgumentInducesNoChanges() {
+        int costB4 = engine.getCost();
+        BigDecimal areaB4 = engine.getArea();
+        BigDecimal supplyB4 = engine.getSupplies();
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            engine.sellLand(Long.MAX_VALUE);
+        });
+
+        String expectedMessage = "Not Enough Land";
+        assertThat(exception.getMessage()).contains(expectedMessage);
+        assertThat(engine.getArea()).isEqualTo(areaB4);
+        assertThat(engine.getSupplies()).isEqualTo(supplyB4);
+        assertThat(engine.getCost()).isEqualTo(costB4);
+    }
+
+    @Test
+    void sellLandRegularUseCaseAndSetPriceToZeroAfterwards() {
+        BigDecimal areaB4 = engine.getArea();
+        BigDecimal supplyB4 = engine.getSupplies();
+        engine.sellLand(4L);
+
+        assertThat(engine.getArea()).isEqualTo(areaB4.subtract(BigDecimal.valueOf(4L)));
+        assertThat(engine.getSupplies()).isEqualTo(supplyB4.add(engine.getYield().multiply(BigDecimal.valueOf(4L))));
+        assertThat(engine.getCost()).isEqualTo(0);
+    }
+
+
+}
