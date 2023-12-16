b6ac0a91bf6d825a2e56b1913f413d35485c73f3
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-16 10:13:12 +0100

    Add doc about colours

--- src/main/java/de/aikiit/game/kaiser/KaiserEnginePrinter.java
+++ src/main/java/de/aikiit/game/kaiser/KaiserEnginePrinter.java
@@ -1,6 +1,8 @@
 package de.aikiit.game.kaiser;
 
 public class KaiserEnginePrinter {
+
+    // inspired by https://talyian.github.io/ansicolors/
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
