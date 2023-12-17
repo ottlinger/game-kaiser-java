bcecced7e40a2c6bdcb2f96bfa2a2bc8fe432200
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-17 23:49:47 +0100

    Site plugin helper

---
+++ site.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Generating new mvn site ..."
+./mvnw clean org.pitest:pitest-maven:mutationCoverage site:site 
+echo "DONE - ready to commit and push"
+cp -rf target/site/* docs
+git add docs/
+git commit -a -m "Update current site"
+git push
