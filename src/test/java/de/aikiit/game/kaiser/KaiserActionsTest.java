90f07042375848847d171c77f1e6c384bb19a5b8
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-19 00:02:30 +0100

    Add more tests

--- src/test/java/de/aikiit/game/kaiser/KaiserActionsTest.java
+++ src/test/java/de/aikiit/game/kaiser/KaiserActionsTest.java
@@ -1,14 +1,33 @@
 package de.aikiit.game.kaiser;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import uk.org.webcompere.systemstubs.SystemStubs;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class KaiserActionsTest {
 
+    @SneakyThrows
     @Test
-    void buy() {
+    void buyWithPositiveInput() {
+        SystemStubs.withTextFromSystemIn("11")
+                .execute(() -> {
+                    assertThat(KaiserActions.buy(new KaiserEngine()))
+                            .isTrue();
+                });
     }
+
+    @SneakyThrows
+    @Test
+    void buyWithZeroInputWillTriggerSell() {
+        SystemStubs.withTextFromSystemIn("0")
+                .execute(() -> {
+                    assertThat(KaiserActions.buy(new KaiserEngine()))
+                            .isFalse();
+                });
+    }
+
 
     @Test
     void sell() {
