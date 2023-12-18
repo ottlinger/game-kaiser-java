bac983863bb30cb0b284153f81645fb082f74141
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-18 23:55:43 +0100

    Prep sys.in tests

---
+++ src/test/java/de/aikiit/game/kaiser/KaiserActionsTest.java
@@ -0,0 +1,24 @@
+package de.aikiit.game.kaiser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KaiserActionsTest {
+
+    @Test
+    void buy() {
+    }
+
+    @Test
+    void sell() {
+    }
+
+    @Test
+    void feed() {
+    }
+
+    @Test
+    void cultivate() {
+    }
+}
