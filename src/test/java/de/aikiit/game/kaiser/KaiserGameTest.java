74a9636cd0383f5326f6f027e600106d00510696
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-18 23:44:24 +0100

    Add basic test for banner printers

---
+++ src/test/java/de/aikiit/game/kaiser/KaiserGameTest.java
@@ -0,0 +1,31 @@
+package de.aikiit.game.kaiser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class KaiserGameTest {
+
+    private KaiserGame game;
+
+    @BeforeEach
+    void createNewGameUnderTest() {
+        this.game = new KaiserGame();
+    }
+
+    @Test
+    void intro() {
+        assertDoesNotThrow(() -> game.intro());
+    }
+
+    @Test
+    void banner() {
+        assertDoesNotThrow(() -> game.banner());
+    }
+
+    @Test
+    void byeByeBanner() {
+        assertDoesNotThrow(() -> game.byeByeBanner());
+    }
+}
