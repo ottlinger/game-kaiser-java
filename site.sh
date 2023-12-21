6c2de74d1508f0c985ca1e56f326e6681f35c399
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-21 11:24:47 +0100

    #3: Wrong issue used before

--- site.sh
+++ site.sh
@@ -4,5 +4,5 @@ echo "Generating new mvn site ..."
 echo "DONE - ready to commit and push"
 cp -rf target/site/* docs
 git add -f docs/
-git commit -a -m "#2: Update current site"
+git commit -a -m "#3: Update current site"
 git push
