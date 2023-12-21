8c0cfc0f1c431ec31c04bedc7bc0421c24599c9b
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-21 21:10:33 +0100

    #3: Add javadoc

--- src/main/java/de/aikiit/game/kaiser/GameKaiserJavaApplication.java
+++ src/main/java/de/aikiit/game/kaiser/GameKaiserJavaApplication.java
@@ -1,5 +1,9 @@
 package de.aikiit.game.kaiser;
 
+/**
+ * This is the main class to start the game.
+ * You can launch it directly or start the shaded JAR.
+ */
 //@SpringBootApplication
 public class GameKaiserJavaApplication {
 
