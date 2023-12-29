1fb72b0ff4697e787a9b8484feb894a2c7404f37
Hugo Hirsch <github@hugo-hirsch.de>
2023-12-29 21:43:20 +0100

    #6: Add maven profile no-tests to provide a fast build

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "Building the game ...."
-./mvnw &> /dev/null
+echo "Building the game without tests...."
+./mvnw -Pno-tests &> /dev/null
 echo "Starting the game ...."
 java -jar target/game.kaiser-0.0.1-SNAPSHOT.jar
